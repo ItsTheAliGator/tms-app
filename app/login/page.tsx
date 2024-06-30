84c4a234d361bf23d380c2b672800179d6fbef69
ItsTheAliGator <alireza.rj.de@hotmail.com>
2024-06-30 10:14:20 +0200

    Changing config

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,6 +15,7 @@ export default function Login({ searchParams }: { searchParams: { message: strin
 		const { error } = await supabase.auth.signInWithOtp({
 			email,
 			options: {
+				shouldCreateUser: false,
 				emailRedirectTo: defaultUrl,
 			},
 		});
